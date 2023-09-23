accf2c69fffb303eb0c99872c47beb8b287faf87
TehWinnie <110377765+TehWinnie@users.noreply.github.com>
2023-09-24 02:03:14 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,26 @@
+var vg_1 = "Death5-.vg.json";
+var vg_2 = "Death5-14.vg.json";
+var vg_3 = "Death15-49.vg.json";
+var vg_4 = "Death50-69.vg.json";
+var vg_5 = "Death70+.vg.json";
+
+vegaEmbed("#choropleth_map1", vg_1).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#choropleth_map2", vg_2).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#choropleth_map3", vg_3).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#choropleth_map4", vg_4).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#choropleth_map5", vg_5).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
